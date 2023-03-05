84a24fce616a9a81e15675d9fcf9b665ecddbb4c
Muukii <muukii.app@gmail.com>
2023-03-05 15:17:15 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftUIHosting",
+  name: "swiftui-hosting",
   platforms: [.iOS(.v13)],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
