4e19c4d2da271059dd42bcb2eb6d6108d35457ac
Muukii <muukii.app@gmail.com>
2023-03-09 14:31:07 +0900

    Add Development project

---
+++ Development/Development/AppDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+import SwiftUI
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    let newWindow = UIWindow()
+    newWindow.rootViewController = UIHostingController.init(rootView: ContentView())
+    newWindow.makeKeyAndVisible()
+    self.window = newWindow
+    return true
+  }
+
+}
