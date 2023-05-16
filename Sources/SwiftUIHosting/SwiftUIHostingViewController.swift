825c5d5c80c6925770b6788eaedb55128bf00350
Muukii <muukii.app@gmail.com>
2023-05-16 13:11:38 +0900

    Patch

---
+++ Sources/SwiftUIHosting/SwiftUIHostingViewController.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+open class SwiftUIHostingViewController: UIViewController {
+
+  public let configuration: SwiftUIHostingView.Configuration
+  private let content: (UIViewController) -> AnyView
+
+  public init<Content: View>(
+    configuration: SwiftUIHostingView.Configuration = .init(),
+    content: @escaping (Self) -> Content
+  ) {
+    self.configuration = configuration
+    self.content = { AnyView(content(unsafeDowncast($0, to: Self.self))) }
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  @available(*, unavailable)
+  public required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  open override func viewDidLoad() {
+
+    let contentView = SwiftUIHostingView(configuration: configuration) { [weak self] in
+      if let self {
+        content(self)
+      }
+    }
+
+    view.addSubview(contentView)
+    contentView.translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate([
+      contentView.topAnchor.constraint(equalTo: view.topAnchor),
+      contentView.rightAnchor.constraint(equalTo: view.rightAnchor),
+      contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      contentView.leftAnchor.constraint(equalTo: view.leftAnchor),
+    ])
+
+  }
+
+}
